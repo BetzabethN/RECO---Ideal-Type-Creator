4f3dd924445d72367741c3ac4343a94432ed087a
Betzabeth Narvaez <betzynarv@gmail.com>
2020-12-13 23:31:17 -0600

    added validation for checkboxes

--- js/custom.js
+++ js/custom.js
@@ -4,3 +4,20 @@ $('#siteNav').affix({
 		top: 100
 	}
 })
+
+// to check that only two pictures are selected
+function ValidatePetSelection()  
+{  
+    var checkboxes = document.getElementsByName("pic_selection");  
+    var numberOfCheckedItems = 0;  
+    for(var i = 0; i < checkboxes.length; i++)  
+    {  
+        if(checkboxes[i].checked)  
+            numberOfCheckedItems++;  
+    }  
+    if(numberOfCheckedItems > 2)  
+    {  
+        alert("사진 2장만 선택하세요!");  
+        return false;  
+    }  
+}  
